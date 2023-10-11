2ac9a231facea3b948f7bb70a168b1495498f528
Chinemere Chukwu <chukwuchinemere@hmail.com>
2023-10-11 11:21:28 +0100

    created the Bug Model

--- bug/models.py
+++ bug/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class bug(models.Model):
+    description = models.CharField(max_length=2000)
+    bug_type = models.CharField(max_length=200)
+    report_date = models.DateTimeField("The date bug is being registered")
+    status = models.CharField(max_length=200)
+
+    def __str__(self) :
+        return f"Bug: {self.description}"
